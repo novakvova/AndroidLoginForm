5fff87c56504d9d08518f1a6f07af3c536737c5a
novakvova <novakvova@gmail.com>
2018-10-23 17:55:35 +0300

    OnClick btnLogin

--- app/src/main/java/com/example/novak/myexampleapp/MainActivity.java
+++ app/src/main/java/com/example/novak/myexampleapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.novak.myexampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login();
+    }
+    public void login() {
+        btn_login = (Button)findViewById(R.id.btnLogin);
+
+        btn_login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,
+                                "Ok",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
